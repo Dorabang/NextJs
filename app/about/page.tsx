8f3fe3b38ec5b38e206c935eb422e111493462fe
Dorabang <psa1195@naver.com>
2023-04-27 20:21:39 +0900

    chore: aboutPage 생성

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
+        quibusdam a odio voluptatum eius tenetur perspiciatis enim labore. Atque
+        voluptas quaerat officiis, numquam eaque vitae minima. Assumenda
+        pariatur consequuntur quidem!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
