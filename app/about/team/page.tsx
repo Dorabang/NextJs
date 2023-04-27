59a9e2aaa83cb677b31d930db0995bb87d64e094
Dorabang <psa1195@naver.com>
2023-04-27 20:21:56 +0900

    chore: teamPage 생성

---
+++ app/about/team/page.tsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+        corporis fuga quaerat quibusdam unde quo exercitationem minima, expedita
+        quasi vitae dolore similique nemo maiores atque non doloribus dolorem
+        libero aperiam?
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
