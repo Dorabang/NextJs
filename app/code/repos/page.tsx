3beafdf100fd0adf3810f5048e901ef225d54e92
Dorabang <psa1195@naver.com>
2023-04-28 22:36:32 +0900

    chore: RepoDir 컴포넌트 wip

---
+++ app/code/repos/page.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
+
+async function fetchRepos() {
+  const response = await fetch(
+    'https://api.github.com/users/bradtraversy/repos'
+  );
+
+  await new Promise((resolve) => setTimeout(resolve, 300)); // wait 1 second
+
+  const repos = await response.json();
+  return repos;
+}
+
+const ReposPage = async () => {
+  const repos = await fetchRepos();
+  console.log(repos);
+
+  interface Repo {
+    id: number;
+    name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    watchers_count: number;
+  }
+
+  return (
+    <div className='repos-container'>
+      <ul className='repo-list'>
+        {repos.map((repo: Repo) => (
+          <li key={repo.id}>
+            <Link href={`/code/repos/${repo.name}`}>
+              <h3>{repo.name}</h3>
+              <p>{repo.description}</p>
+              <div className='repo-detail'>
+                <span>
+                  <FaStar />
+                  {repo.stargazers_count}
+                </span>
+                <span>
+                  <FaCodeBranch />
+                  {repo.forks_count}
+                </span>
+                <span>
+                  <FaEye />
+                  {repo.watchers_count}
+                </span>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ReposPage;
